3cc83b571fc770b8a05fb19ab81f30a3944df610
Alice <Alice@c-link.com.tw>
2025-10-02 18:05:12 +0800

    feat: 新增LoginPage和product list

---
+++ src/page/LoginPage.jsx
@@ -0,0 +1,60 @@
+import axios from "axios";
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+
+export default function LoginPage({ account, setAccount }) {
+  const handleInputChange = (e) => {
+    setAccount({
+      ...account,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+
+    try {
+      const res = await axios.post(
+        `${BASE_URL}/admin/signin`,
+        account
+      );
+      
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <>
+      <div className="d-flex flex-column justify-content-center align-items-center vh-100">
+        <h1 className="mb-5">請先登入</h1>
+        <form onSubmit={handleSubmit} className="d-flex flex-column gap-3">
+          <div className="form-floating mb-3">
+            <input
+              type="email"
+              className="form-control"
+              id="username"
+              placeholder="name@example.com"
+              value={account.username}
+              name="username"
+              onChange={handleInputChange}
+            />
+            <label htmlFor="username">Email address</label>
+          </div>
+          <div className="form-floating">
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              placeholder="Password"
+              value={account.password}
+              name="password"
+              onChange={handleInputChange}
+            />
+            <label htmlFor="password">Password</label>
+          </div>
+          <button type="submit" className="btn btn-primary">登入</button>
+        </form>
+      </div>
+    </>
+  );
+}
