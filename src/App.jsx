f524090e2233d86c949a461090045f36af541ec9
Alice <Alice@c-link.com.tw>
2025-10-09 18:44:18 +0800

    feat: add ShoppingPage

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 
 import LoginPage from "./page/LoginPage";
 import { ProductPage } from "./page/ProductPage";
+import ShoppingPage from "./page/ShoppingPage"
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+const API_PATH = import.meta.env.VITE_API_PATH;
 
 function App() {
   const [isAuth, setIsAuth] = useState(false);
@@ -22,8 +25,22 @@ function App() {
   //   checkUserLogin();
   // }, []);
 
+  const addCartItem = async(product_id, qty) => {
+    try {
+      await axios.post(`${BASE_URL}/api/${API_PATH}/cart`, {
+        data: {
+          product_id,
+          qty: Number(qty)
+        }
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
-    <>{isAuth ? <ProductPage /> : <LoginPage setIsAuth={setIsAuth}/>}</>
+    // <>{isAuth ? <ProductPage /> : <LoginPage setIsAuth={setIsAuth}/>}</>
+    <ShoppingPage></ShoppingPage>
   );
 }
 
